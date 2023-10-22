b4ca4e7f1ab36068f03b86444408e653a0b89d5a
Sugar1307 <2270563h@gsuite.kobe-u.ac.jp>
2023-10-22 14:04:26 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,19 @@
 // 小手調べ
 function setup(){
+  createCanvas(1000,1000);
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let d = (i+1)*20;
+    
+    if(i<5){
+      stroke(0, 0, 255);
+    } 
+    else{
+      stroke(255, 0, 0);
+    }
+    ellipse(100, 100, d, d);
+    
+  
   }
+ 
 }
