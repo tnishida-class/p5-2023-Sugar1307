5306c9c68c1aefd5594991e2a1d2de9105463b8f
Sugar1307 <146808316+Sugar1307@users.noreply.github.com>
2023-11-27 10:22:40 +0900

    Change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -3,11 +3,13 @@
 function setup(){
   createCanvas(400, 400);
   background(240);
+  let pie_w = 300;
+  let pie_h = 300;
 
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
@@ -15,4 +17,12 @@ function setup(){
   for(let i = 0; i < scores.length; i++){ total += scores[i]; }
 
   // BLANK[1]
+  for(let  i = 0; i < scores.length; i++){
+    let lastAngle = HALF_PI;
+    let angle = scores[i]/total * 100;
+    arc(width/2, height/2, pie_w, pie_h, radians(lastAngle), radians(lastAngle+angle))
+    lastAngle += angle;
+  }
+  
 }
+
