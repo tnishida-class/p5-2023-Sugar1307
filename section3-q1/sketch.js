7cf1934e3b5c8b23f189870df0aa254573455fff
Sugar1307 <146808316+Sugar1307@users.noreply.github.com>
2023-11-27 10:21:23 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,28 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let speed;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  speed = 1;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
+  count = (count + speed) % cycle; //countを０から１００の間で増加させる(どんどん大きくなる）
+  if (keyIsPressed){
+    speed = 2;
+  } else{
+    speed = 1;
+  } //キーを押しているときは速くなる
+  if(count<cycle/2){
+    size=count+50;
+  } //countが周期の半分未満の時は円を大きくする
+  else{
+    size=(cycle-count)+50;
+  } //countが周期の半分以上のときは円を小さくする
   ellipse(width / 2, height / 2, size);
 }
